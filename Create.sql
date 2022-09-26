cdfb2451517e5a4c68dc5cbc7f18dabeb70fb801
Vitória <81492871+Viimbb@users.noreply.github.com>
2022-09-26 15:11:22 -0300

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -5,4 +5,11 @@ CREATE TABLE Persons (
     FirstName varchar (255)
     Address varchar (255)
     City varchar (255)
-)
+);
+
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+); 
