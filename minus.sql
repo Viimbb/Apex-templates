e5e9ef652053ee7581bef96ba6e09ea2c320becb
Vitória <81492871+Viimbb@users.noreply.github.com>
2022-10-13 13:13:12 -0300

    Update minus.sql

--- minus.sql
+++ minus.sql
@@ -1,12 +1,11 @@
 SELECT votacao, secao FROM adm b
 MINUS
-SELECT ... FROM.... a;
-
+SELECT ... FROM... a;
 
 
 count=$((FIRSTV-SECONDV))
-count=$(expr $FIRSTV - $SECONDV);
-
+count=$(expr $FIRSTV - $SECONDV)
+count=$(
 
 
 Jdbc.getConnection("jdbc:oracle:thin:@ipaddress:1521:SID","username","password");
