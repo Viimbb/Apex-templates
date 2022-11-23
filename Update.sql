f1b283452a3355349f4854cab0188556465c6824
Vitória <81492871+Viimbb@users.noreply.github.com>
2022-11-23 14:21:39 -0300

    Update Update.sql

--- Update.sql
+++ Update.sql
@@ -4,20 +4,11 @@ select * from order by dt_cadastro_date desc
 
 
 
-UPDATE admestagiario.desenv_tab_estagiario
-SET DSCLOGRAD_ENDERECO = '386', COMPLEMENTO_ENDERECO= Apartamento A, FONEPRINC='98121-6897', GRAUINSTR='Ensino Médio Completo', AREAATUACAO= 'Ciencias Contabeis '
-WHERE Alessandra Ágata Miranda Vaz Pereira;  
+UPDATE nomedatabela
+SET nomedacoluna = '', nomedacoluna= , nomedacoluna=''
+WHERE nome_aluno;  
 
 
-
-
-UPDATE admestagiario.desenv_tab_estagiario
-SET FONEPRINC='991475051', GRAUINSTR='Ensino Médio Completo', AREAATUACAO= 'Artes Visuais'
-WHERE MATRICULA= EST_0000001008;  
-
-
-
-
-UPDATE desenv_tab_estagiario
-SET Telefone = '991475051',
-WHERE ID_ESTAGIO = '1008';
+UPDATE nomedatabela
+SET  curso= 'ENSINO MEDIO'
+WHERE nomedacoluna = '';
