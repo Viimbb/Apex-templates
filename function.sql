1022db0d36c7306f895b0c83a84eb8531c5ef5ac
Vitória <81492871+Viimbb@users.noreply.github.com>
2023-01-20 17:11:42 -0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,1 +1,10 @@
 create or replace FUNCTION fc_..._... (p_matricula  varchar2, p_hora varchar2)
+
+    -- Verifica se existe alguma exceção cadastrada no respectivo horário
+    -- ######################################
+    select count(*)
+      into v_possui_excecao
+      from S_EXCECAO_HORARIO
+     where ativo = 1
+       and mat_sr = p_matricula
+       and v_data_ponto between data_inicio and data_fim;
